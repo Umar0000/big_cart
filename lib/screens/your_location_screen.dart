792f33580a1048c8ca524a1f5abf02879b34c14d
umar.ahmed <umar.ahmed@avanzasolutions.com>
2023-06-10 12:26:29 +0500

    working complete

---
+++ lib/screens/your_location_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class YorLocationScreen extends StatefulWidget {
+  const YorLocationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<YorLocationScreen> createState() => _YorLocationScreenState();
+}
+
+class _YorLocationScreenState extends State<YorLocationScreen> {
+  @override
+  void initState() {
+
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
